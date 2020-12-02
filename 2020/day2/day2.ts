5d50279e498574e4c421ef211925b083707df7bb
Wilgert Velinga <wilgert@wilgert.nl>
2020-12-02 10:30:35 +0100

    prettify day2

--- 2020/day2/day2.ts
+++ 2020/day2/day2.ts
@@ -1,41 +1,52 @@
-import {readFile} from "../../common/readFile";
+import { readFile } from "../../common/readFile";
 
-const prepareInput = (rawInput: string) => rawInput
+const prepareInput = (rawInput: string) => rawInput;
 
-const input = prepareInput(readFile(__dirname + '/input.txt'))
+const input = prepareInput(readFile(__dirname + "/input.txt"));
 
 interface PasswordAndPolicy {
-  min: number,
-  max: number,
-  letter: string,
-  password: string
+  min: number;
+  max: number;
+  letter: string;
+  password: string;
 }
 
 const goB = (input): number => {
-  return createPasswordsAndPolicies(input)
-    .filter((passwordAndPolicy) => isValidPart2(passwordAndPolicy)).length;
-}
+  return createPasswordsAndPolicies(input).filter((passwordAndPolicy) =>
+    isValidPart2(passwordAndPolicy)
+  ).length;
+};
 
-function isValidPart1({min, max, letter, password}: PasswordAndPolicy) {
-  let length = password.split('').filter(l => l === letter).length;
+function isValidPart1({ min, max, letter, password }: PasswordAndPolicy) {
+  let length = password.split("").filter((l) => l === letter).length;
   return length >= min && length <= max;
 }
 
-function isValidPart2({min, max, letter, password}: PasswordAndPolicy) {
-  return (password[min-1] === letter && password[max-1] !== letter) || (password[min-1] !== letter && password[max-1] === letter)
+function isValidPart2({ min, max, letter, password }: PasswordAndPolicy) {
+  return (
+    (password[min - 1] === letter && password[max - 1] !== letter) ||
+    (password[min - 1] !== letter && password[max - 1] === letter)
+  );
 }
 
 function createPasswordsAndPolicies(input): PasswordAndPolicy[] {
-  return input.split('\n')
-    .filter(line => line)
-    .map(line => line.match(/(\d{1,2})-(\d{1,2}) ([a-z]): (.*)/))
-    .map(([, min, max, letter, password]) => ({min: parseInt(min), max: parseInt(max), letter, password}));
+  return input
+    .split("\n")
+    .filter((line) => line)
+    .map((line) => line.match(/(\d{1,2})-(\d{1,2}) ([a-z]): (.*)/))
+    .map(([, min, max, letter, password]) => ({
+      min: parseInt(min),
+      max: parseInt(max),
+      letter,
+      password,
+    }));
 }
 
 const goA = (input): number => {
-  return createPasswordsAndPolicies(input)
-    .filter((passwordAndPolicy) => isValidPart1(passwordAndPolicy)).length;
-}
+  return createPasswordsAndPolicies(input).filter((passwordAndPolicy) =>
+    isValidPart1(passwordAndPolicy)
+  ).length;
+};
 
 /* Tests */
 
@@ -43,10 +54,10 @@ const goA = (input): number => {
 
 /* Results */
 
-console.time("Time")
-const resultA = goA(input)
-const resultB = goB(input)
-console.timeEnd("Time")
+console.time("Time");
+const resultA = goA(input);
+const resultB = goB(input);
+console.timeEnd("Time");
 
-console.log("Solution to part 1:", resultA)
-console.log("Solution to part 2:", resultB)
+console.log("Solution to part 1:", resultA);
+console.log("Solution to part 2:", resultB);
