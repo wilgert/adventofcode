07fb130384d8d22af15b7004db81ed726df43d8a
Wilgert Velinga <wilgert@wilgert.nl>
2020-12-03 09:46:00 +0100

    day 3 completed

---
+++ 2020/day3/day3.ts
@@ -0,0 +1,67 @@
+import { readFile } from "../../common/readFile";
+
+const prepareInput = (rawInput: string) =>
+  rawInput.split("\n").map((row) => row.split(""));
+
+const input = prepareInput(readFile(__dirname + "/input.txt"));
+
+function countTrees(
+  map: string[][],
+  { right, down }: { right: number; down: number }
+) {
+  let trees = 0;
+  for (let i = 0; i < map.length; i += down ) {
+    let row = map[i];
+    let treeIndex = (i/down * right) % row.length;
+    const tree = row[treeIndex];
+    if (tree === "#") {
+      trees++;
+    }
+  }
+  return trees;
+}
+
+const goA = (input): number => {
+  return countTrees(input, { right: 3, down: 1 });
+};
+
+const goB = (input) => {
+  const slopes = [
+    {
+      right: 1,
+      down: 1,
+    },
+    {
+      right: 3,
+      down: 1,
+    },
+    {
+      right: 5,
+      down: 1,
+    },
+    {
+      right: 7,
+      down: 1,
+    },
+    {
+      right: 1,
+      down: 2,
+    },
+  ];
+
+  return slopes.reduce((totalTrees, slope) => totalTrees * countTrees(input, slope), 1)
+};
+
+/* Tests */
+
+// test()
+
+/* Results */
+
+console.time("Time");
+const resultA = goA(input);
+const resultB = goB(input);
+console.timeEnd("Time");
+
+// console.log("Solution to part 1:", resultA);
+console.log("Solution to part 2:", resultB);
