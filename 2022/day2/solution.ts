f0e3bfeca7a7ce69585c427facf8b073cf949918
Wilgert Velinga <wilgert@wilgert.nl>
2022-12-02 12:08:21 +0100

    2022 day2 - refactored to make it more DRY

--- 2022/day2/solution.ts
+++ 2022/day2/solution.ts
@@ -6,61 +6,75 @@ B X
 C Z
 `);
 
-type Round = { firstCol: 'A' | 'B' | 'C', secondCol: 'X' | 'Y' | 'Z' };
+type Round = { firstCol: "A" | "B" | "C"; secondCol: "X" | "Y" | "Z" };
 
 function processInput(input: string): Round[] {
   return input
     .split("\n")
     .filter((i) => i)
-    .map(move => {
-      const [firstCol, secondCol] = move.split(' ');
-      return {firstCol, secondCol} as Round;
-    })
+    .map((move) => {
+      const [firstCol, secondCol] = move.split(" ");
+      return { firstCol, secondCol } as Round;
+    });
 }
 
-function countScorePart1({firstCol, secondCol}: Round) {
+function countScorePart1({ firstCol, secondCol }: Round) {
   const moveScoreMap = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3
-  }
+    X: 1,
+    Y: 2,
+    Z: 3,
+  };
 
   const resultScoreMap = {
-    'AX': 3,
-    'AY': 6,
-    'AZ': 0,
-    'BX': 0,
-    'BY': 3,
-    'BZ': 6,
-    'CX': 6,
-    'CY': 0,
-    'CZ': 3,
-  }
+    AX: 3,
+    AY: 6,
+    AZ: 0,
+    BX: 0,
+    BY: 3,
+    BZ: 6,
+    CX: 6,
+    CY: 0,
+    CZ: 3,
+  };
 
   return moveScoreMap[secondCol] + resultScoreMap[`${firstCol}${secondCol}`];
 }
 
-function countScorePart2({firstCol, secondCol}: Round) {
-  const resultScoreMap = {
-    'AX': 3 + 0,
-    'AY': 1 + 3,
-    'AZ': 2 + 6,
-    'BX': 1 + 0,
-    'BY': 2 + 3,
-    'BZ': 3 + 6,
-    'CX': 2 + 0,
-    'CY': 3 + 3,
-    'CZ': 1 + 6
-  }
+function countScorePart2({ firstCol, secondCol }: Round) {
+  const roundToMyMoveMap = {
+    AX: "C",
+    AY: "A",
+    AZ: "B",
+    BX: "A",
+    BY: "B",
+    BZ: "C",
+    CX: "B",
+    CY: "C",
+    CZ: "A",
+  };
 
-  return resultScoreMap[`${firstCol}${secondCol}`];
+  const moveScoreMap = {
+    A: 1,
+    B: 2,
+    C: 3,
+  };
+
+  const resultScoreMap = {
+    X: 0,
+    Y: 3,
+    Z: 6,
+  };
+
+  return (
+    moveScoreMap[roundToMyMoveMap[`${firstCol}${secondCol}`]] +
+    resultScoreMap[`${secondCol}`]
+  );
 }
 
 function part1(input) {
   const scores = input.map(countScorePart1);
   return scores.reduce((total, score) => score + total);
 }
-
 
 function part2(input) {
   const scores = input.map(countScorePart2);
