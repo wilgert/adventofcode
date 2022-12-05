ec4a7bba8a6306031ee788fe7231b9ec0f955a55
Wilgert Velinga <wilgert@wilgert.nl>
2022-12-05 09:56:51 +0100

    2022 day5

---
+++ 2022/day5/solution.ts
@@ -0,0 +1,87 @@
+import { readFile } from "../../common/readFile";
+
+const input = readFile(__dirname + "/input.txt");
+const example = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+`;
+
+function processStacks(stacks: string): string[][] {
+  return stacks
+    .split("\n")
+    .filter((i) => i)
+    .reduce((result, row, currentIndex) => {
+      for (let i = 0; i < 9; i++) {
+        if (!result[i]) {
+          result[i] = [];
+        }
+
+        let crate = row.slice(i * 4, i * 4 + 3);
+        if (crate && crate.includes("[")) {
+          result[i].push(crate);
+        }
+      }
+
+      return result;
+    }, []);
+}
+
+type Move = { from: number; to: number; count: number };
+
+function processMoves(moves: string): Move[] {
+  return moves
+    .split("\n")
+    .filter((i) => i)
+    .map((move) => {
+      const [, count, from, to] = move
+        .match(/move (\d+) from (\d) to (\d)/)
+        .map((m) => parseInt(m, 10));
+      return { count, from, to };
+    });
+}
+
+function processInput(input: string): { stacks: string[][]; moves: Move[] } {
+  const [stacks, moves] = input.split("\n\n");
+
+  let stacks1 = processStacks(stacks); // ?
+  return { stacks: stacks1, moves: processMoves(moves) };
+}
+
+function part1(input) {
+  const {stacks, moves} = processInput(input);
+  moves.forEach(({ count, from, to }) => {
+    for (let i = 0; i < count; i++) {
+      stacks[to - 1].unshift(stacks[from - 1].shift());
+    }
+  });
+
+  return stacks
+    .map((stack) => stack[0])
+    .join("")
+    .replace(/\[|\]/gm, "");
+}
+
+function part2(input) {
+  const {stacks, moves} = processInput(input);
+
+  moves.forEach(({ count, from, to }) => {
+    const removed = stacks[from-1].splice(0, count);
+    stacks[to - 1].unshift(...removed);
+  });
+
+  return stacks
+    .map((stack) => stack[0])
+    .join("")
+    .replace(/\[|\]/gm, "");
+}
+
+part1(example); // ?
+part1(input); // ?
+part2(example); // ?
+part2(input); // ?
