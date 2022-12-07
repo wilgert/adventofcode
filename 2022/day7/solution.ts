856c0a588c500d3a7ba99fd3d1f3a13f569cdfe9
Wilgert Velinga <wilgert@wilgert.nl>
2022-12-07 16:49:19 +0100

    2022 day7 original chatGPT solution with lots of bugs

--- 2022/day7/solution.ts
+++ 2022/day7/solution.ts
@@ -1,53 +1,23 @@
 import { readFile } from "../../common/readFile";
 
 const input = readFile(__dirname + "/input.txt");
-const example = `$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
+const example = `
+
 `
-function dfs(node: Node, callback: (node: Node) => void) {
-  callback(node);
-  for (const child of node.children.values()) {
-    dfs(child, callback);
-  }
-}
 
 class Node {
+  name: string;
+  size: number;
   children: Map<string, Node>;
-  parent: Node;
 
-  constructor(public readonly name: string, public size: number) {
+  constructor(name: string, size: number) {
+    this.name = name;
+    this.size = size;
     this.children = new Map<string, Node>();
   }
 
   addChild(child: Node) {
-    child.parent = this;
-
     this.children.set(child.name, child);
-  }
-
-  getTotalSize() {
-    return [...this.children.values()].reduce((total, child) => total + child.getTotalSize(), this.size);
   }
 }
 
@@ -55,16 +25,14 @@ class DirectoryTree {
   root: Node;
   current: Node;
 
-  constructor(input: string[]) {
+  constructor() {
     this.root = new Node("/", 0);
     this.current = this.root;
-    this.parseInput(input);
   }
 
-  private parseInput(input: string[]) {
-    for (let i = 0; i < input.length; i++){
-      const line = input[i];
-      if (line && line.startsWith("$ cd ")) {
+  parseInput(input: string[]) {
+    for (const line of input) {
+      if (line.startsWith("$ cd ")) {
         const directory = line.slice(5);
         if (directory === "/") {
           this.current = this.root;
@@ -76,64 +44,40 @@ class DirectoryTree {
           }
           this.current = this.current.children.get(directory);
         }
-      } else if (line && line.startsWith("$ ls")) {
-        const nextCommandLine = input.findIndex((line, index) => index > i && line.startsWith(`$ `)) ;
-
-        let sliceEnd = input.length;
-        if(nextCommandLine > 0) {
-          sliceEnd = nextCommandLine;
-        }
-
-        const files = input.slice(i + 1, sliceEnd);
+      } else if (line.startsWith("$ ls")) {
+        const files = line.slice(4).split(" ");
         for (const file of files) {
-          const [dirOrSize, name] = file.split(" ");
-
-          if(dirOrSize === 'dir'){
+          const [name, size] = file.split(".");
+          if (size) {
+            this.current.size += parseInt(size, 10);
+          } else {
             if (!this.current.children.has(name)) {
               this.current.addChild(new Node(name, 0));
             }
-          } else {
-            this.current.size += parseInt(dirOrSize, 10);
           }
-        }
-        if(nextCommandLine > 0) {
-          i = nextCommandLine - 1;
         }
       }
     }
   }
+
+  getTotalSize(directory: string): number {
+    if (!this.current.children.has(directory)) {
+      return 0;
+    }
+    const node = this.current.children.get(directory);
+    return node.size + Array.from(node.children.values()).reduce((acc, child) => acc + this.getTotalSize(child.name), 0);
+  }
 }
 
 function part1(input) {
-  const tree = new DirectoryTree(input.split(`\n`).filter(i => i));
-  const result = [];
-  tree.current = tree.root;
-  dfs(tree.root, (node) => {
-    const size = node.getTotalSize();
-    if (size <= 100000) {
-      result.push(size);
-    }
-  });
-  return result.reduce((t, i) => t + i);
+  return new DirectoryTree(input);
 }
 
 function part2(input) {
-  const tree = new DirectoryTree(input.split(`\n`).filter(i => i));
-  const result = [];
-  tree.current = tree.root;
-
-  dfs(tree.root, (node) => {
-    const size = node.getTotalSize();
-    result.push(size);
-  });
-
-  const total = tree.root.getTotalSize();
-  const free = 70000000 - total;
-  const needed = 30000000
-  return Math.min(...result.filter(size => size > needed - free ));
+  return findFirstMarker(input, 14);
 }
 
-part1(example); // ?
 part1(input); // ?
-part2(example); // ?
+part1(example); // ?
 part2(input); // ?
+part2(example); // ?
